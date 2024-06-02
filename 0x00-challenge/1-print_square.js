a48dd9bbedeb1af1d5431341999274b739d459b8
hichamb2 <beihaqihicham@gmail.com>
2024-06-03 00:17:30 +0100

    commit for revising

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -15,6 +15,11 @@ if (process.argv.length <= 2) {
 }
 
 size = parseInt(process.argv[2])
+if (isNaN(size) || size <= 0)
+	{
+		process.stderr.write("Invalid size\n");
+		process.exit(1);
+	}
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
