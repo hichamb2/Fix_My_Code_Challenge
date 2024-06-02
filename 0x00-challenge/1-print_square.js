4a0fd3451361ffacb68cbc7f79b190aacfc904c0
hichamb2 <beihaqihicham@gmail.com>
2024-06-03 00:23:30 +0100

    commit for revising

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -7,23 +7,21 @@
 */
 
 
-if (process.argv.length <= 2) {
+if (process.argv.length <= 2)
+{
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
 
-size = parseInt(process.argv[2])
-if (isNaN(size) || size <= 0)
-	{
-		process.stderr.write("Invalid size\n");
-		process.exit(1);
-	}
+size = parseInt(process.argv[2], 10)
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+for (let i = 0 ; i < size ; i ++)
+{
+	for (let j = 0 ; j < size ; j ++)
+	{
+		process.stdout.write("#");
+	}
+	process.stdout.write("\n");
 }
